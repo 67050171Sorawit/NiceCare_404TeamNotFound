8bd5fba9d775faf60dcba031dc3477f14d1dd528
sorawit <67050171@go.buu.ac.th>
2026-05-28 10:31:21 +0700

    update fall detection module

--- data-ai/fall_detection.py
+++ data-ai/fall_detection.py
@@ -1,9 +1,24 @@
 import cv2
 import mediapipe as mp
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# =========================
+# FIREBASE
+# =========================
+
+cred = credentials.Certificate("firebase_key.json")
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://nice-care-4fa00-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+
 # =========================
 # MEDIAPIPE SETUP
 # =========================
+
 mp_pose = mp.solutions.pose
 mp_draw = mp.solutions.drawing_utils
 
@@ -12,6 +27,7 @@ pose = mp_pose.Pose()
 # =========================
 # CAMERA
 # =========================
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -30,6 +46,8 @@ while True:
     # AI PROCESS
     results = pose.process(rgb)
 
+    fall_status = "NORMAL"
+
     # DRAW SKELETON
     if results.pose_landmarks:
 
@@ -37,6 +55,43 @@ while True:
             frame,
             results.pose_landmarks,
             mp_pose.POSE_CONNECTIONS
+        )
+
+        # =========================
+        # FALL DETECTION
+        # =========================
+
+        shoulder = results.pose_landmarks.landmark[11]
+        hip = results.pose_landmarks.landmark[23]
+
+        diff_y = abs(shoulder.y - hip.y)
+
+        # ถ้าลำตัวแนวนอนเกินไป
+        if diff_y < 0.1:
+            fall_status = "FALL DETECTED"
+
+        # =========================
+        # SEND TO FIREBASE
+        # =========================
+
+        ref = db.reference('/fall_status')
+
+        ref.set({
+            'status': fall_status
+        })
+
+        # =========================
+        # SHOW STATUS
+        # =========================
+
+        cv2.putText(
+            frame,
+            fall_status,
+            (30, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 255),
+            3
         )
 
     # SHOW
